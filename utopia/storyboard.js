678b3f0a7e538597e958fb632e3ef11db9eda4ef
Troy Robinson <troy@lionsrock.com>
2025-04-10 17:39:25 -0600

    Resolving conflicts during rebase

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -1,24 +1,9 @@
 import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
 import { App } from '../src/app'
-import { Playground } from '../src/playground'
 
 export var storyboard = (
   <Storyboard>
-    <Scene
-      id='playground-scene'
-      commentId='playground-scene'
-      style={{
-        width: 700,
-        height: 759,
-        position: 'absolute',
-        left: 212,
-        top: 128,
-      }}
-      data-label='Playground'
-    >
-      <Playground style={{}} />
-    </Scene>
     <Scene
       id='app-scene'
       commentId='app-scene'
@@ -31,7 +16,7 @@ export var storyboard = (
       }}
       data-label='My App'
     >
-      <App style={{}} />
+      <App />
     </Scene>
   </Storyboard>
 )
